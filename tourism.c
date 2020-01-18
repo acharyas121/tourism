26f88b1dfb36f354c9724052d88d6bcf5b50a5c8
acharyas121 <47661795+acharyas121@users.noreply.github.com>
2020-01-18 17:48:56 +0545

    Add files via upload

---
+++ tourism.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+  char x,y;
+  printf("\n\n\n\t\t\t\t\t\tAre you a tourist or an official?\n\n\t\t\t\t\t Press 'T' if you are toursit.\n\n\n\t\t\t\t\t\t Press 'O' if you are an official");
+  scanf("%s%s",&x,&y );
+
+
+printf("________________________________________\n");
+printf("|       Hello                           |\n");
+printf("|        Welcome   To My Pogram         |\n");
+printf("|_______________________________________|\n");
+}
